4e04518559e350c7105476e3589fc028d750827f
EgydioBNeto <egydiobolonhezi@gmail.com>
2025-04-07 17:59:59 -0300

    Debuggando o envio de logs

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,10 +1,18 @@
 import sdk from './tracer';
 sdk.start();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app
+    .listen(3001)
+    .then(() => {
+      console.log('Aplicação subiu!');
+    })
+    .catch(() => {
+      console.error('Aplicação não subiu!');
+  });
 }
 bootstrap();
--- app/src/tracer.ts
+++ app/src/tracer.ts
@@ -16,7 +16,7 @@ import {
 } from '@opentelemetry/semantic-conventions';
 import { SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs';
 import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-proto';
-// import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 
 const traceExporter = new OTLPTraceExporter();
 
@@ -25,10 +25,8 @@ const resource = resourceFromAttributes({
   [ATTR_SERVICE_VERSION]: '1.0.0',
 });
 
-console.log('cheguei aqui!');
-
 const mergedResource = resource;
-// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
 const sdk = new NodeSDK({
   spanProcessor: new BatchSpanProcessor(traceExporter),
