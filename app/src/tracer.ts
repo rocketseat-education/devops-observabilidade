97ed0b56df17e806aeff683fb4d6000ac36a5d6c
EgydioBNeto <egydiobolonhezi@gmail.com>
2025-04-07 17:47:55 -0300

    Configurando a aplicação com OTEL

---
+++ app/src/tracer.ts
@@ -0,0 +1,44 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import {
+  BatchSpanProcessor,
+  ConsoleSpanExporter,
+} from '@opentelemetry/sdk-trace-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import {
+  PeriodicExportingMetricReader,
+  ConsoleMetricExporter,
+} from '@opentelemetry/sdk-metrics';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import {
+  ATTR_SERVICE_NAME,
+  ATTR_SERVICE_VERSION,
+} from '@opentelemetry/semantic-conventions';
+import { SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs';
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-proto';
+// import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+
+const traceExporter = new OTLPTraceExporter();
+
+const resource = resourceFromAttributes({
+  [ATTR_SERVICE_NAME]: 'app-rocketseat',
+  [ATTR_SERVICE_VERSION]: '1.0.0',
+});
+
+console.log('cheguei aqui!');
+
+const mergedResource = resource;
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+const sdk = new NodeSDK({
+  spanProcessor: new BatchSpanProcessor(traceExporter),
+  traceExporter: new ConsoleSpanExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter(),
+  }),
+  logRecordProcessor: new SimpleLogRecordProcessor(new OTLPLogExporter()),
+  instrumentations: [getNodeAutoInstrumentations()],
+  resource: mergedResource,
+});
+
+export default sdk;
