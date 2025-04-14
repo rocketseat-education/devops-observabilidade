8155887ca69a91a847f085f396ee40df45128440
EgydioBNeto <egydiobolonhezi@gmail.com>
2025-04-14 15:02:25 -0300

    Debuggando algumas métricas auto instrumentadas

--- app/src/tracer.ts
+++ app/src/tracer.ts
@@ -17,11 +17,21 @@ import {
 import { SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs';
 import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-proto';
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
 
+const SERVICE_NAME = 'app-rocketseat';
+
+const metricsExporter = new OTLPMetricExporter({
+  url: 'http://127.0.0.1:4317',
+});
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: metricsExporter,
+  exportIntervalMillis: 10000,
+});
 const traceExporter = new OTLPTraceExporter();
 
 const resource = resourceFromAttributes({
-  [ATTR_SERVICE_NAME]: 'app-rocketseat',
+  [ATTR_SERVICE_NAME]: SERVICE_NAME,
   [ATTR_SERVICE_VERSION]: '1.0.0',
 });
 
@@ -29,14 +39,11 @@ const mergedResource = resource;
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
 const sdk = new NodeSDK({
-  spanProcessor: new BatchSpanProcessor(traceExporter),
-  traceExporter: new ConsoleSpanExporter(),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
-  }),
-  logRecordProcessor: new SimpleLogRecordProcessor(new OTLPLogExporter()),
+  traceExporter,
+  metricReader,
   instrumentations: [getNodeAutoInstrumentations()],
   resource: mergedResource,
+  serviceName: SERVICE_NAME,
 });
 
 export default sdk;
