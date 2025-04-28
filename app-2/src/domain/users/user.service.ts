9868f72a20e11d9cd10da1634bfec96e6506bab6
EgydioBNeto <egydiobolonhezi@gmail.com>
2025-04-28 19:26:51 -0300

    Analisando os traces distribuídos

--- app-2/src/domain/users/user.service.ts
+++ app-2/src/domain/users/user.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { log } from '../../infra/logger'
 
 @Injectable()
 export class UserService {
   list(): Array<{ name: string, email: string }> {
+      log.info ('Buscando usuários...');
       return [
         {
           name: "User 1", 
--- app/src/domain/users/user.service.ts
+++ app/src/domain/users/user.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { log } from '../../infra/logger'
 import { request } from 'undici';
 
 @Injectable()
 export class UserService {
   async list(): Promise<any> {
-      const { statusCode, body } = await request('http://localhost:3002/users');
+      const { body } = await request('http://localhost:3002/users');
       const payload = await body.json();
-      log.info (`${statusCode} | ${JSON.stringify(payload)}`);
+      return payload;
   }
 }
