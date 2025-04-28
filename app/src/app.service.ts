57b29482d107e699550b127b2314485f3cc24a3f
EgydioBNeto <egydiobolonhezi@gmail.com>
2025-04-28 18:21:24 -0300

    Ajustando e testando o fluxo

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { metrics } from '@opentelemetry/api';
+import { log } from './infra/logger';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     const metric = metrics.getMeter('app-rocketseat');
     const successMetric = metric.createCounter('hello_success');
+    log.info('Cheguei aqui!');
     successMetric.add(1);
     return 'Olá Rocketseat!';
   }
